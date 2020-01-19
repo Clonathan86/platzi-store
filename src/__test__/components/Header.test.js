4621b2cd67fe453746d68989178f22aeb59159fa
Clonathan86 <forerojonathan@gmail.com>
2020-01-18 21:46:53 -0500

    Create snaphots for Footer and Header test suits

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
+import { create } from 'react-test-renderer';
 
 describe('<Header />', () => {
   const provider =
@@ -17,5 +18,16 @@ describe('<Header />', () => {
   test('Title render', () => {
     const header = mount(provider);
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('Component UI check', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
